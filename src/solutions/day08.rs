7414e114714a3b51ca8bf1d4de0e728e3afca778
dennis <dennisheperol@gmail.com>
2023-02-01 23:45:39 +0100

    day 8 part 2

--- src/solutions/day08.rs
+++ src/solutions/day08.rs
@@ -12,7 +12,10 @@ pub fn part1() {
 pub fn part2() {
     let content = fs::read_to_string("puzzle_input/day08.txt").expect("Couldn't read file");
 
-    println!("{content}");
+    let trees = parse_trees(content);
+    let largest = trees.get_largest_scenic_score();
+
+    println!("{largest}");
 }
 
 #[derive(Debug)]
@@ -47,6 +50,54 @@ impl Trees {
             self.trees.push(vec![]);
         }
         self.trees[row].push(Tree::new(height));
+    }
+
+    fn get_largest_scenic_score(&self) -> usize {
+        let mut largest = 0;
+
+        for row in 1..(self.trees.len()-1) {
+            for col in 1..(self.trees[row].len()-1){
+                let scenic_score = self.get_scenic_score(row, col);
+                if scenic_score > largest {
+                    largest = scenic_score;
+                }
+            }
+        }
+
+        largest
+    }
+
+    fn get_scenic_score(&self, tree_row: usize, tree_col: usize) -> usize {
+
+        let tree = &self.trees[tree_row][tree_col];
+
+        let mut left = 0;
+        for comp_col in (0..tree_col).rev() {
+            left += 1;
+            let comp_tree = &self.trees[tree_row][comp_col];
+            if comp_tree.height >= tree.height { break; }
+        }
+        let mut right = 0;
+        for comp_col in (tree_col + 1)..self.trees[tree_row].len() {
+            right += 1;
+            let comp_tree = &self.trees[tree_row][comp_col];
+            if comp_tree.height >= tree.height { break; }
+        }
+
+        let mut up = 0;
+        for comp_row in (0..tree_row).rev() {
+            up += 1;
+            let comp_tree = &self.trees[comp_row][tree_col];
+            if comp_tree.height >= tree.height { break; }
+        }
+        let mut down = 0;
+        for comp_row in (tree_row + 1)..self.trees.len() {
+            down += 1;
+            let comp_tree = &self.trees[comp_row][tree_col];
+            if comp_tree.height >= tree.height { break; }
+        }
+
+        left * right * up * down
     }
 
     fn count_visible_trees(&mut self) -> usize {
